940a337c3b30f8a2e534927df850f8e00ae67b9e
Szymon <szymqwerty@gmail.com>
2020-11-09 23:17:26 +0100

    test1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res)=>{
     res.status(200).json({msg: 'success'})
