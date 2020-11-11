01ab4e80e7f5ba245df3b1f5d6082a7abebad443
Szymon <szymqwerty@gmail.com>
2020-11-11 22:00:29 +0100

    front-back

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const path = require('path');
 
 
 const sum = require('./sum');
@@ -14,7 +14,7 @@ app.use(helmet())
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res)=>{
+app.get('/api', (req,res)=>{
     res.status(200).json({msg: 'success'})
 })
 
@@ -23,7 +23,13 @@ app.get('/test', (req,res)=>{
 })
 
 
-console.log(123);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    );
+  }
 
 app.listen(PORT, ()=>{
     console.log('works')
